27aad52ccd29c64763a25297b78583fd6995890b
jskodova <89833620+jskodova@users.noreply.github.com>
2022-02-09 16:51:45 +0200

    created header.php

---
+++ UpdatedNorthTravelWebsite/header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Business Frontpage - Start Bootstrap Template</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid d-flex align-items-center justify-content-between">
+              <div class="leftnav container-md mb-0 pb-0 col-md-5 col-lg-5 col-sm-12">
+                  <a class="navbar-brand" href="index.html">
+                      <img src="images/logo.png" alt="logo North Travel" class="logo-image">
+                  </a>
+                  <span class="navbar-brand navbar-text">North Travel - Explore the Nordics</span>
+              </div>
+            <div class="midnav navbar-brand container-md" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="C:\Users\PC1\Pictures\destination.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Destinations</a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="destination.html">Our selection</a></li>
+                    <li><a class="dropdown-item" href="swedenpage.html">Sweden</a></li>
+                    <li><a class="dropdown-item" href="icelandpage.html">Iceland</a></li>
+                    <li><a class="dropdown-item" href="norwaypage.html">Norway</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="ContactUs.html">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+            <div class="rightnav container-md" id="navbarSupportedContent">
+              <form class="d-flex">
+                  <div class="form"> <i class="fa fa-search"></i> <input type="text" class="form-control form-input" placeholder="Search..."> <span class="left-pan"><i class="fa fa-microphone"></i></span> </div>
+              </form>
+            </div>
+          </div>
+        </nav>
+          <!-- Responsive header-->
+          <header class="header">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="images/norway.png" class="d-block w-100 img-fluid" alt="Norway destination">
+                    <div class="carousel-caption">
+                        <h2 class="text-uppercase">Norway</h2>
+                      </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/sweden.png" class="d-block w-100 img-fluid" alt="Sweden destination">
+                    <div class="carousel-caption">
+                        <h2 class="text-uppercase">Sweden</h2>
+                      </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/iceland.png" class="d-block w-100 img-fluid" alt="Iceland destination">
+                    <div class="carousel-caption">
+                        <h2 class="text-uppercase">Iceland</h2>
+                      </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+            </header>
