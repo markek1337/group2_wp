584b7815a786d68d2572151601221e84c30087fa
markek1337 <petrovv.mark00@gmail.com>
2022-02-09 17:04:56 +0200

    footer.php added

--- UpdatedNorthTravelWebsite/ContactUs.php
+++ UpdatedNorthTravelWebsite/ContactUs.php
@@ -59,38 +59,7 @@ include "header.php";?>
 </body>
 
   <!--Footer-->
-  <footer>
-    <div class="footer-top">
-        <div class="container fot-cont">
-            <div class="row my-row-4">
-                <div class="col-md-4 col-sm-6 col-xs-12 segment-one">
-                    <img src="images/newlogo.png" class="img footer-logo" alt="" width="127" height="106">
-                    <h2>NORTH TRAVEL</h2>
-                </div>
-
-                <div class="col-md-4 col-sm-6 col-xs-12 segment-two">
-                    <img src="images/social media icons.png" class="img social-media" alt="" width="257" height="70">
-                </div>
-
-                <div class="col-md-4 col-sm-6 col-xs-12 segment-three">
-                    <h2>SIGN UP FOR NEWSLETTER</h2>
-                    <form class="input-group">
-                        <input type="text" class="form-control form-control-sm" placeholder="Your email"
-                          aria-label="Your email" aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                          <button class="btn btn-sm btn-outline-white my-0" type="button">Sign Up</button>
-                        </div>
-                      </form>
-                  </div>
-                </div>
-            </div>
-            <div class="row my-row-4">
-                <div class="col-md-12 segment-four">
-                    <p>&copy; North Travel 2022 All Rights Reserved</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
+<?php
+include "footer.php";?>
 
 </html>
--- UpdatedNorthTravelWebsite/icelandpage.php
+++ UpdatedNorthTravelWebsite/icelandpage.php
@@ -69,38 +69,6 @@ include "header.php";?>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 
-    <footer>
-        <div class="footer-top">
-            <div class="container fot-cont">
-                <div class="row my-row-4">
-                    <div class="col-md-4 col-sm-6 col-xs-12 segment-one">
-                        <img src="images/newlogo.png" class="img footer-logo" alt="" width="127" height="106">
-                        <h2 class="footer-text">NORTH TRAVEL</h2>
-                    </div>
-    
-                    <div class="col-md-4 col-sm-6 col-xs-12 segment-two">
-                        <img src="images/social media icons.png" class="img social-media" alt="" width="257" height="70">
-                    </div>
-    
-                    <div class="col-md-4 col-sm-6 col-xs-12 segment-three">
-                        <h2>SIGN UP FOR NEWSLETTER</h2>
-                        <form class="input-group">
-                            <input type="text" class="form-control form-control-sm" placeholder="Your email"
-                              aria-label="Your email" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                              <button class="btn btn-sm btn-outline-white my-0" type="button">Sign Up</button>
-                            </div>
-                          </form>
-                      </div>
-                    </div>
-                </div>
-                <div class="row my-row-4">
-                    <div class="col-md-12 segment-four">
-                        <p>&copy; North Travel 2022 All Rights Reserved</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-
+<?php
+include "footer.php";?>
   </html>
