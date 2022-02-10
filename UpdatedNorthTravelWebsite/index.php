f71abc0e45f6db91672da3bf1a32da9d7ed3f528
jskodova <89833620+jskodova@users.noreply.github.com>
2022-02-10 18:36:08 +0200

    updated index.php

--- UpdatedNorthTravelWebsite/index.php
+++ UpdatedNorthTravelWebsite/index.php
@@ -1,5 +1,37 @@
 <?php
 include "header.php";?>
+              <!-- Responsive header-->
+          <header class="header">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="images/norway.png" class="d-block w-100 img-fluid" alt="Norway destination">
+                    <div class="carousel-caption">
+                        <h2 class="text-uppercase">Norway</h2>
+                      </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/sweden.png" class="d-block w-100 img-fluid" alt="Sweden destination">
+                    <div class="carousel-caption">
+                        <h2 class="text-uppercase">Sweden</h2>
+                      </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/iceland.png" class="d-block w-100 img-fluid" alt="Iceland destination">
+                    <div class="carousel-caption">
+                        <h2 class="text-uppercase">Iceland</h2>
+                      </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+            </header>
     <body>
         <!-- Responsive navbar-->
         <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
