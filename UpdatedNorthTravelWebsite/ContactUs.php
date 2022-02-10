1b5fe582bf347c7886cf6cc9601515d95af47949
markek1337 <petrovv.mark00@gmail.com>
2022-02-10 19:00:46 +0200

    added website

---
+++ UpdatedNorthTravelWebsite/ContactUs.php
@@ -0,0 +1,97 @@
+<?php
+include "header.php";
+?>
+
+      <div class="row">
+        <div class="col-12">
+            <main>
+          <img width="100%" height="647px" src="https://res.cloudinary.com/anora/image/upload/f_auto,q_auto/v1612346750/common/other/northern-lights.jpg" alt="main image" >
+           </div>
+      </div>
+
+      
+
+      <div class="grid-container">
+        <div class="grid-item"> </div>
+        <div class="grid-item"><h1><strong>Contact Us</strong></h1> </div>
+        <div class="grid-item"> </div>
+
+        <div class="grid-item"> 
+           <img width="100px" height="100px" alt="location" src="https://img.icons8.com/ios/344/place-marker--v1.png">
+        </div>
+        <div class="grid-item">
+           <img width="100px" height="100px" alt="phone" src="https://img.icons8.com/ios/344/phone.png">
+        </div>
+        <div class="grid-item">
+            <img width="100px" height="100px" alt="email" src="https://img.icons8.com/ios/344/email-open.png">
+        </div>
+        <div class="grid-item"><b>Location</b></div>
+        <div class="grid-item"><b>Phone number</b></div>
+        <div class="grid-item"><b>E-mail</b></div>  
+        <div class="grid-item">Pohjoisesplanadi<br>
+            11-13 00170<br>
+            Helsinki</div>
+        <div class="grid-item">+35825657316</div>
+        <div class="grid-item">northtravel@gmail.com</div> 
+      </div>
+      
+    </main>
+    </div>
+
+    <aside>
+    <div id=forms>
+        <form action="/action_page.php">
+          <label for="fname">First Name</label>
+          <input type="information" id="fname" name="firstname" placeholder="Your name..">
+      
+          <label for="lname">Last Name</label>
+          <input type="information" id="lname" name="lastname" placeholder="Your last name..">
+      
+          <label for="sbj">Subject</label>
+          <input type="information" id="sbj" name="subject" placeholder="Write your request..">
+        
+          <input type="submit" value="Submit">
+        </form>
+      </div>
+    </aside>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+</body>
+
+  <!--Footer-->
+  <footer>
+    <div class="footer-top">
+        <div class="container fot-cont">
+            <div class="row my-row-4">
+                <div class="col-md-4 col-sm-6 col-xs-12 segment-one">
+                    <img src="images/newlogo.png" class="img footer-logo" alt="" width="127" height="106">
+                    <h2>NORTH TRAVEL</h2>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12 segment-two">
+                    <img src="images/social media icons.png" class="img social-media" alt="" width="257" height="70">
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12 segment-three">
+                    <h2>SIGN UP FOR NEWSLETTER</h2>
+                    <form class="input-group">
+                        <input type="text" class="form-control form-control-sm" placeholder="Your email"
+                          aria-label="Your email" aria-describedby="basic-addon2">
+                        <div class="input-group-append">
+                          <button class="btn btn-sm btn-outline-white my-0" type="button">Sign Up</button>
+                        </div>
+                      </form>
+                  </div>
+                </div>
+            </div>
+            <div class="row my-row-4">
+                <div class="col-md-12 segment-four">
+                    <p>&copy; North Travel 2022 All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</html>
---
+++ UpdatedNorthTravelWebsite/destination.php
@@ -0,0 +1,134 @@
+<?php
+include "header.php";
+?>
+
+        <!--Map Image-->
+
+        <img src="images/map_destination_page.png" class="img-responsive" alt="" width="100%">
+
+        <!--Heading-->
+        <div class="heading">
+            <h1 style="font: size 48px; text-align: center;"><b>Travel the Nordics with Us</b></h1>
+        </div>
+
+        <!--Main Content-->
+        <div class="container my-container">
+
+
+            <!--Iceland-->
+            <div class="row justify-content-center my-row-1">
+                <div class="col-md-4 justify-content-center my-col-1">
+                        <img src="images/destination1-iceland.jpg" class="img-destinations img-fluid" alt="">
+                </div>
+
+                <div class="col-md-7 my-col-2">
+                    <div class="iceland-article">
+                        <h1>Iceland</h1>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vivamus aliquet purus convallis eleifend viverra. Suspendisse
+                        non tortor vitae nisi ultricies laoreet a ac justo.
+                        Suspendisse quis lacinia leo. Donec tortor risus,
+                        interdum ut massa a, pharetra blandit massa.
+                        Suspendisse consequat fermentum metus, at maximus mauris posuere vitae.
+                        Phasellus vitae iaculis lorem, at accumsan lorem.
+                        In hendrerit volutpat nisi eget luctus. Phasellus a pharetra dui.
+                        <br>
+                        <a href="icelandpage.html" class="link my-link">Learn more</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <!--Sweden-->
+            <div class="row justify-content-center my-row-2">
+                <div class="col-md-7 my-col-3">
+                    <div class="sweden-article">
+                        <h1>Sweden</h1>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vivamus aliquet purus convallis eleifend viverra. Suspendisse
+                        non tortor vitae nisi ultricies laoreet a ac justo.
+                        Suspendisse quis lacinia leo. Donec tortor risus,
+                        interdum ut massa a, pharetra blandit massa.
+                        Suspendisse consequat fermentum metus, at maximus mauris posuere vitae.
+                        Phasellus vitae iaculis lorem, at accumsan lorem.
+                        In hendrerit volutpat nisi eget luctus. Phasellus a pharetra dui.
+                        <br>
+                        <a href="swedenpage.html" class="link my-link">Learn more</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 d-flex justify-content-end my-col-4">
+                    <img src="images/destination2-sweden.jpg" class="img-destinations img-fluid" alt="">
+                </div>
+            </div>
+
+
+            <!--Norway-->
+            <div class="row justify-content-center my-row-3">
+                <div class="col-md-4 justify-content-center my-col-5">
+                    <img src="images/destination3-norway.jpg" class="img-destinations img-fluid" alt="">
+                </div>
+
+                <div class="col-md-7 my-col-6">
+                    <div class="norway-article">
+                        <h1>Norway</h1>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vivamus aliquet purus convallis eleifend viverra. Suspendisse
+                        non tortor vitae nisi ultricies laoreet a ac justo.
+                        Suspendisse quis lacinia leo. Donec tortor risus,
+                        interdum ut massa a, pharetra blandit massa.
+                        Suspendisse consequat fermentum metus, at maximus mauris posuere vitae.
+                        Phasellus vitae iaculis lorem, at accumsan lorem.
+                        In hendrerit volutpat nisi eget luctus. Phasellus a pharetra dui.
+                        <br>
+                        <a href="norwaypage.html" class="link my-link">Learn more</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!--Footer-->
+        <footer>
+            <div class="footer-top">
+                <div class="container fot-cont">
+                    <div class="row my-row-4">
+                        <div class="col-md-4 col-sm-6 col-xs-12 segment-one">
+                            <img src="images/newlogo.png" class="img footer-logo" alt="" width="127" height="106">
+                            <h2 class="footer-text">NORTH TRAVEL</h2>
+                        </div>
+
+                        <div class="col-md-4 col-sm-6 col-xs-12 segment-two">
+                            <img src="images/social media icons.png" class="img social-media" alt="" width="257" height="70">
+                        </div>
+
+                        <div class="col-md-4 col-sm-6 col-xs-12 segment-three">
+                            <h2>SIGN UP FOR NEWSLETTER</h2>
+                            <form class="input-group">
+                                <input type="text" class="form-control form-control-sm" placeholder="Your email"
+                                  aria-label="Your email" aria-describedby="basic-addon2">
+                                <div class="input-group-append">
+                                  <button class="btn btn-sm btn-outline-white my-0" type="button">Sign Up</button>
+                                </div>
+                              </form>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="row my-row-4">
+                        <div class="col-md-12 segment-four">
+                            <p>&copy; North Travel 2022 All Rights Reserved</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    </body>
+</html>
