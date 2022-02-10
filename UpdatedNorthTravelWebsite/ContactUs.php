da23278395dad371914fe1d98d4acea15ee17943
markek1337 <petrovv.mark00@gmail.com>
2022-02-10 18:19:43 +0200

    incoming changes

--- UpdatedNorthTravelWebsite/ContactUs.php
+++ UpdatedNorthTravelWebsite/ContactUs.php
@@ -1,56 +1,5 @@
-<<<<<<< HEAD
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
-
-    </style>
-  </head>
-  <body>
-    <!-- Navbar -->
-    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid d-flex align-items-center justify-content-between">
-          <div class="leftnav container-md mb-0 pb-0 col-md-5 col-lg-5 col-sm-12">
-              <a class="navbar-brand" href="index.html">
-                  <img src="images/logo.png" alt="logo North Travel" class="logo-image">
-              </a>
-              <span class="navbar-brand navbar-text">North Travel - Explore the Nordics</span>
-          </div>
-        <div class="midnav navbar-brand container-md" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="C:\Users\PC1\Pictures\destinations.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Destinations</a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="destination.html">Our selection</a></li>
-                <li><a class="dropdown-item" href="swedenpage.html">Sweden</a></li>
-                <li><a class="dropdown-item" href="icelandpage.html">Iceland</a></li>
-                <li><a class="dropdown-item" href="norwaypage.html">Norway</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="ContactUs.html">Contact Us</a>
-            </li>
-          </ul>
-        </div>
-        <div class="rightnav container-md" id="navbarSupportedContent">
-          <form class="d-flex">
-              <div class="form"> <i class="fa fa-search"></i> <input type="text" class="form-control form-input" placeholder="Search..."> <span class="left-pan"><i class="fa fa-microphone"></i></span> </div>
-          </form>
-        </div>
-      </div>
-    </nav>
-=======
 <?php
 include "header.php";?>
->>>>>>> 584b7815a786d68d2572151601221e84c30087fa
 
       <div class="row">
         <div class="col-12">
--- UpdatedNorthTravelWebsite/index.php
+++ UpdatedNorthTravelWebsite/index.php
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Business Frontpage - Start Bootstrap Template</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="style.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        
-    </head>
-=======
 <?php
 include "header.php";?>
->>>>>>> 584b7815a786d68d2572151601221e84c30087fa
     <body>
         <!-- Responsive navbar-->
         <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
