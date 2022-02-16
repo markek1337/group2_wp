d4a6b83f92c373f07d75496adfe60f117ead05c2
mona21000 <98873477+mona21000@users.noreply.github.com>
2022-02-16 13:25:04 +0200

    commit

--- UpdatedNorthTravelWebsite/myJS.js
+++ UpdatedNorthTravelWebsite/myJS.js
@@ -4,3 +4,10 @@ function setColor (where, newColor) {
     else
     document.body.style.color = newColor;
 }
+function myjsfunction() {
+    document.getElementById("demo").onclick = function() {myjsfunction()};
+
+    function myjsfunction() {
+      document.getElementById("demo").innerHTML = "Your request has been submitted.";
+    }
+}
