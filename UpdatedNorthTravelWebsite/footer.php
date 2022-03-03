2661c510a0540f58fdcd0416c5b05d1977356189
mona21000 <98873477+mona21000@users.noreply.github.com>
2022-03-03 17:47:51 +0200

    Newsletter & database connection

--- UpdatedNorthTravelWebsite/footer.php
+++ UpdatedNorthTravelWebsite/footer.php
@@ -1,3 +1,19 @@
+<?php include 'connection.php'; 
+include 'connection.php'; 
+if(isset($_POST['submit'])){
+    $email = $_POST['email'];
+    $sql="INSERT INTO Newsletter (Email) values ('$email')";
+    $result = mysqli_query($con,$sql);
+    if($result){
+        echo "You are now subscribed";
+    
+    }else{
+        die(mysqli_error($con));
+    }
+    
+    }
+    
+?>
 <footer>
             <div class="footer-top">
                 <div class="container fot-cont">
@@ -12,12 +28,26 @@
                         </div>
 
                         <div class="col-md-4 col-sm-6 col-xs-12 segment-three">
+
+                        <script>  
+function validateemail()  
+{  
+var x=document.Newsletter.email.value;  
+var atposition=x.indexOf("@");  
+var dotposition=x.lastIndexOf(".");  
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+  alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
+  return false;  
+  }  
+}  
+</script> 
                             <h2>SIGN UP FOR NEWSLETTER</h2>
-                            <form class="input-group">
-                                <input type="text" class="form-control form-control-sm" placeholder="Your email"
-                                  aria-label="Your email" aria-describedby="basic-addon2">
+                            <form class="input-group" name= Newsletter method = "post" action="#" onsubmit="return validateemail();">
+                            <label >Email</label>
+                          <input type="email" class="form-control form-control-sm" placeholder="Enter your email..." name="email" aria-label="Your email" aria-describedby="basic-addon2">
+
                                 <div class="input-group-append">
-                                  <button class="btn btn-sm btn-outline-white my-0" type="button">Sign Up</button>
+                                  <button class="btn btn-sm btn-outline-white my-0" type="submit" name="submit">Sign Up</button>
                                 </div>
                               </form>
                           </div>
