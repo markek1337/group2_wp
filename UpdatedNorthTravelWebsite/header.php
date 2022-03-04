b2f00e4ba9c71cd02c129ce5f52b5524b1dd524a
jskodova <89833620+jskodova@users.noreply.github.com>
2022-03-04 10:18:28 +0200

    updated header

--- UpdatedNorthTravelWebsite/header.php
+++ UpdatedNorthTravelWebsite/header.php
@@ -61,10 +61,17 @@
                 </li>
                 <!-- Themes-->
                 <li>
-                  <a class="btn btn-primary nav-btn" href="multi_login/register/register.php" role="button" style="background-color:#1cb49a; margin-left: 100%; border:none; padding:10px; margin-top:5px;">Register</a>
-                  <a class="btn btn-primary nav-btn" href="PROFILE/profilepage.php" role="button" style="background-color:#4f7fa3; border:none; padding:10px; margin-top:5px;">My profile</a>
-                  <a class="btn btn-primary nav-btn" href="multi_login/filter/filter.php" role="button" style="background-color:#4f7fa3; border:none; padding:10px; margin-top:5px;">Filter Search</a>
+                <?php if (isset($_SESSION['success'])) : ?>
+                  <a class="btn btn-primary nav-btn" href="Login/register/register.php" role="button" style="background-color:#1cb49a; margin-left: 30%; border:none; padding:10px; margin-top:5px;">Profile</a>
+                  <a class="btn btn-primary nav-btn" href="Login/filter/filter.php" role="button" style="background-color:#4f7fa3; border:none; padding:10px; margin-top:5px;">Cart</a>
+                  <a class="btn btn-primary nav-btn" href="Login/filter/filter.php" role="button" style="background-color:#4f7fa3; border:none; padding:10px; margin-top:5px;">Filter Search</a>
+                  <a class="btn btn-primary nav-btn" href="Login/filter/filter.php" role="button" style="background-color:#4f7fa3; border:none; padding:10px; margin-top:5px;">Log out</a>
 
+
+                <?php else : ?>
+                  <a class="btn btn-primary nav-btn" href="Login/register/register.php" role="button" style="background-color:#1cb49a; margin-left: 100%; border:none; padding:10px; margin-top:5px;">Register</a>
+                  <a class="btn btn-primary nav-btn" href="Login/filter/filter.php" role="button" style="background-color:#4f7fa3; border:none; padding:10px; margin-top:5px;">Filter Search</a>
+                 <?php endif ?>
                 </li>
               </ul>
             </div>
